041a5b2b9326172ea2843e6735a6636fedf839c2
Real Solly M <12409199+RealSollyM@users.noreply.github.com>
2026-09-17 11:30:49 +0200

    chore: add pnpm workspace configuration to allow builds for specific packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,19 +3,19 @@ import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import AutoImport from "astro-auto-import";
-import { defineConfig, squooshImageService } from "astro/config";
+import { defineConfig } from "astro/config";
 import remarkCollapse from "remark-collapse";
 import remarkToc from "remark-toc";
 import config from "./src/config/config.json";
 
 // https://astro.build/config
 export default defineConfig({
-  site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
+  site: config.site.base_url
+    ? config.site.base_url
+    : "https://www.barebliss.co.za",
   base: config.site.base_path ? config.site.base_path : "/",
   trailingSlash: config.site.trailing_slash ? "always" : "never",
-  image: {
-    
-  },
+  image: {},
   integrations: [
     react(),
     sitemap(),
